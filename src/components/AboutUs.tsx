5ab27aebfa430a80bbbc2f0c4309e5f40fc6f15e
Wᴀᴋᴀꜱᴀ 托曼 <ankitchoudhary2005@gmail.com>
2026-07-18 13:38:45 +0530

    Add files via upload

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUs: React.FC = () => {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>Paradise Nursery is your premier destination for beautiful houseplants, succulents, and flowering plants. We offer a wide selection of high-quality plants to bring life and greenery into your home.</p>
+      <p>Our mission is to make plant care accessible and enjoyable for everyone. Whether you're a seasoned plant parent or just starting your green journey, we have something for you.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
